d36c44296e9509598039b808f0e2a7f2b75a80d9
rh01 <1048157315@qq.com>
2020-11-27 21:30:04 +0800

    refactore server coed
    Signed-off-by: rh01 <1048157315@qq.com>

---
+++ Documents/webassmbly/cmd/wasm/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"syscall/js"
+)
+
+func main() {
+	fmt.Println("Go Web assembly")
+	js.Global().Set("formatJSON", jsonWrapper())
+	<-make(chan struct{})
+}
+
+func prettyJSON(input string) (string, error) {
+	var raw interface{}
+	if err := json.Unmarshal([]byte(input), &raw); err != nil {
+		return "", err
+	}
+	pretty, err := json.MarshalIndent(raw, "", " ")
+	if err != nil {
+		return "", err
+	}
+	return string(pretty), nil
+}
+
+//expose Go function to jsfunction
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(
+		func(this js.Value, args []js.Value) interface{} {
+			if len(args) != 1 {
+				return "Invalid no of arguments passed"
+			}
+			inputJSON := args[0].String()
+			fmt.Printf("input %s\n", inputJSON)
+			pretty, err := prettyJSON(inputJSON)
+			if err != nil {
+				fmt.Printf("unable to convert to json%s\n", err)
+				return err.Error()
+			}
+			return pretty
+		})
+	return jsonFunc
+}
